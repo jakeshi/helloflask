d9b5ff707dcdb79ef54ea4b63fa09c020acffab8
Jake <jakehku@gmail.com>
2015-07-21 22:20:05 -0400

    Update hello.py
    2nd edit

--- hello.py
+++ hello.py
@@ -3,6 +3,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+    
 @app.route('/')
 def hello():
     return 'Hello World!'
